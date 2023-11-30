afb3e81cfbdab9908bb921d4b3bc443d90f26e4e
Camilo Caro <camilo@Camilos-MacBook-Air.local>
2023-11-30 16:17:25 -0500

    [DEV] first commit and add dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './services/theme.service';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NzButtonModule, NzIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'movies-app';
+  
+  constructor(private themeService: ThemeService) {}
+
+  toggleTheme(): void {
+    console.log('theme changed');
+    this.themeService.toggleTheme().then();
+  }
 }
